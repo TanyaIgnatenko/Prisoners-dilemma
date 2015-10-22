c5854a5dd62ee04836e10a46ea4e973c59afec1e
14203_ignatenko <14203_ignatenko@59651650-e24a-48e3-812e-a1f3cb8db93f>
2015-10-22 19:46:58 +0000

    Considered situation when some strategies has equal max scores in tournament

--- trunk/dilemma/user.cc
+++ trunk/dilemma/user.cc
@@ -14,21 +14,27 @@ bool compare(const pair<string, int> & a, const pair<string, int> & b)
 
 void user::print_results_of_tour(std::map<string, int> & sum_of_scores, std::vector<string> & name_of_strategy, int & count_of_strategies) const
 {
+	cout << "Scores of tour:" << endl;
 	std::vector<std::pair<string, int> > scores(count_of_strategies);
 	std::map<string, int>::iterator ptr = sum_of_scores.begin();
 	for (int i = 0; i < count_of_strategies; ++i, ++ptr)
 	{
 		scores[i] = *ptr;
 	}
-
 	std::sort(scores.begin(), scores.end(), compare);
 	for (int i = 0; i < count_of_strategies; ++i)
 	{
 		cout << scores[i].first << " " << scores[i].second << "\n";
 	}
 
-	cout << "The Winer: " <<  scores[count_of_strategies - 1].first << " " << scores[count_of_strategies - 1].second << "\n";
-
+	int idx = scores.size() - 1;
+	cout <<"\n" << "The Winer/s: " << endl;
+	cout << scores[idx].first << " " << scores[idx].second << "\n"; 
+	for (int i = scores.size() - 2; scores[i].second == scores[i+1].second; --i) //when some strategies has equal amount of scores
+	{
+		 cout << scores[i].first << " " << scores[i].second << "\n"; 
+	}
+	cout << "" << endl;
 }
 
 
--- trunk/dilemma/user_interface.cc
+++ trunk/dilemma/user_interface.cc
@@ -69,13 +69,13 @@ void user::user_interface(vector<string> & name_of_strategy, int count_of_strate
 		}
 		for (int i = 0; i <  count_of_strategies; ++i)
 		{
-			for (int j = i + 1; j < count_of_strategies; ++i)
+			for (int j = i + 1; j < count_of_strategies; ++j)
 			{
 				for (int k = j + 1; k < count_of_strategies; ++k)
 				{
 
 					Game game(name_of_strategy[i], name_of_strategy[j], name_of_strategy[k]);
-					
+
 					for (int i = 0; i < steps; ++i)
 					{
 						game.tick();
@@ -85,7 +85,7 @@ void user::user_interface(vector<string> & name_of_strategy, int count_of_strate
 					sum_of_scores[name_of_strategy[i]] += game.get_game_scores(1);
 					sum_of_scores[name_of_strategy[j]] += game.get_game_scores(2);
 					sum_of_scores[name_of_strategy[k]] += game.get_game_scores(3);
-
+					
 					print_results_of_tour(sum_of_scores, name_of_strategy, count_of_strategies);
 				}
 			}
