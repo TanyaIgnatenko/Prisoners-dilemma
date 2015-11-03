7a6a99c3ab4f1ce75f439d8fd74c9765edc690ba
14203_ignatenko <TanyaIgnatenko@users.noreply.github.com>
2015-11-03 17:40:03 +0000

    Separated responsability between func

--- main.cc
+++ main.cc
@@ -3,5 +3,11 @@
 
 int main(int argc, const char **argv)
 { 	
-	return (User::instance()->command_line_parser(argc, argv) == Result::Success);
+	Parser_Result result = User::instance()->command_line_parser(argc, argv);
+	if(result.success)
+	{
+		return (User::instance()->user_interface(result.strategy_name, result.mode, result.steps, result.rules) == Result::Success);
+		
+	}
+	return 1;
 }
