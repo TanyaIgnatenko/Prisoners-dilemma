4d91250c61b46516100aff041755c4004fb63142
14203_ignatenko <TanyaIgnatenko@users.noreply.github.com>
2015-11-03 16:09:13 +0000

    Fixed Bugs

--- game.cc
+++ game.cc
@@ -8,29 +8,63 @@
 #include "matrix.h"
 #include "strategy.h"
 
-void erase_digits(std::string & name)
+using std::string;
+using std::vector;
+
+void erase_digits(string & name)
 {
 	size_t found = name.find_last_of("_");
-	if(found!=std::string::npos)
+	if(found!=string::npos)
 	{
 		name.erase(name.begin() + found, name.end());
 	}
 }
 
-Game::Game(std::string n1, std::string n2, std::string n3, matrix & sc) : scores(n1, n2, n3)
+Game::Game(string n1, string n2, string n3, const vector<vector <int> > & rules) : scores(n1, n2, n3), rules_(rules)
 {
-	if(!sc.is_empty())
+	if(!rules_.size())
 	{
-		scores = sc;
-	}	
+		rules_.resize(3);
+		for(auto & v : rules_)
+		{
+			v.resize(8);
+		}
+		rules_[0][0] = 4;
+		rules_[0][1] = 2;
+		rules_[0][2] = 2;
+		rules_[0][3] = 5;
+		rules_[0][4] = 0;
+		rules_[0][5] = 3;
+		rules_[0][6] = 3;
+		rules_[0][7] = 1;
+
+		rules_[1][0] = 4;
+		rules_[1][1] = 2;
+		rules_[1][2] = 5;
+		rules_[1][3] = 2;
+		rules_[1][4] = 3;
+		rules_[1][5] = 0;
+		rules_[1][6] = 3;
+		rules_[1][7] = 1;
+
+		rules_[2][0] = 4;
+		rules_[2][1] = 5;
+		rules_[2][2] = 2;
+		rules_[2][3] = 2;
+		rules_[2][4] = 3;
+		rules_[2][5] = 3;	
+		rules_[2][6] = 0;
+		rules_[2][7] = 1;
+	}
+
 	erase_digits(n1);
 	erase_digits(n2);
 	erase_digits(n3);
 	try
 	{
-		prisoner1 = Factory<std::string, Strategy>::instance()->create(n1);
-		prisoner2 = Factory<std::string, Strategy>::instance()->create(n2);
-		prisoner3 = Factory<std::string, Strategy>::instance()->create(n3);
+		prisoner1 = Factory<string, Strategy>::instance()->create(n1);
+		prisoner2 = Factory<string, Strategy>::instance()->create(n2);
+		prisoner3 = Factory<string, Strategy>::instance()->create(n3);
 	}
 	catch(...)
 	{
@@ -41,7 +75,7 @@ Game::Game(std::string n1, std::string n2, std::string n3, matrix & sc) : scores
 
 void Game::tick()
 {
-	std::vector<choice> choices(3);
+	vector<choice> choices(3);
 	choices[0] = prisoner1->decide();
 	choices[1] = prisoner2->decide();
 	choices[2] = prisoner3->decide();
@@ -50,36 +84,36 @@ void Game::tick()
 	prisoner2->enemy_choices(choices[0], choices[2]);
 	prisoner3->enemy_choices(choices[0], choices[1]);
 
-	if(choices[0] == choice::Betray &&  choices[1] == choice::Betray &&  choices[2] == choice::Betray)
+	if(choices[0] == choice::RemainSilent &&  choices[1] == choice::RemainSilent &&  choices[2] == choice::RemainSilent)
 	{
-		scores.add_scores(1, 1, 1);
-	}
-	else if(choices[0] == choice::Betray &&  choices[1] == choice::Betray &&  choices[2] == choice::RemainSilent)
-	{
-		scores.add_scores(3, 3, 0);
-	}
-	else if(choices[0] == choice::Betray &&  choices[1] == choice::RemainSilent &&  choices[2] == choice::Betray)
-	{
-		scores.add_scores(3, 0, 3);
-	}
-	else if(choices[0] == choice::RemainSilent &&  choices[1] == choice::Betray &&  choices[2] == choice::Betray)
-	{
-		scores.add_scores(0, 3, 3);
-	}
-	else if(choices[0] == choice::Betray &&  choices[1] == choice::RemainSilent &&  choices[2] == choice::RemainSilent)
-	{
-		scores.add_scores(5, 2, 2);
-	}
-	else if(choices[0] == choice::RemainSilent &&  choices[1] == choice::Betray &&  choices[2] == choice::RemainSilent)
-	{
-		scores.add_scores(2, 5, 2);
+		scores.add_scores(rules_[0][0], rules_[1][0], rules_[2][0]);
 	}
 	else if(choices[0] == choice::RemainSilent &&  choices[1] == choice::RemainSilent &&  choices[2] == choice::Betray)
 	{
-		scores.add_scores(2, 2, 5);
+		scores.add_scores(rules_[0][1], rules_[1][1], rules_[2][1]);
 	}
-	else if(choices[0] == choice::RemainSilent &&  choices[1] == choice::RemainSilent &&  choices[2] == choice::RemainSilent)
+	else if(choices[0] == choice::RemainSilent &&  choices[1] == choice::Betray &&  choices[2] == choice::RemainSilent)
 	{
-		scores.add_scores(4, 4, 4);
+		scores.add_scores(rules_[0][2], rules_[1][2], rules_[2][2]);
+	}
+	else if(choices[0] == choice::Betray &&  choices[1] == choice::RemainSilent &&  choices[2] == choice::RemainSilent)
+	{
+		scores.add_scores(rules_[0][3], rules_[1][3], rules_[2][3]);
+	}
+	else if(choices[0] == choice::RemainSilent &&  choices[1] == choice::Betray &&  choices[2] == choice::Betray)
+	{
+		scores.add_scores(rules_[0][4], rules_[1][4], rules_[2][4]);
+	}
+	else if(choices[0] == choice::Betray &&  choices[1] == choice::RemainSilent &&  choices[2] == choice::Betray)
+	{
+		scores.add_scores(rules_[0][5], rules_[1][5], rules_[2][5]);
+	}
+	else if(choices[0] == choice::Betray &&  choices[1] == choice::Betray &&  choices[2] == choice::RemainSilent)
+	{
+		scores.add_scores(rules_[0][6], rules_[1][6], rules_[2][6]);
+	}
+	else if(choices[0] == choice::Betray &&  choices[1] == choice::Betray &&  choices[2] == choice::Betray)
+	{
+		scores.add_scores(rules_[0][7], rules_[1][7], rules_[2][7]);
 	}
 }
--- matrix.h
+++ matrix.h
@@ -15,12 +15,8 @@ public:
 	matrix(){}
 	matrix(string & n1, string & n2, string & n3) : strategy_name1(n1), strategy_name2(n2), strategy_name3(n3), empty(0){}
 
-	matrix(const matrix & other) : strategy_name1(other.strategy_name1), strategy_name2(other.strategy_name2)\
-	, strategy_name3(other.strategy_name3), scores1(other.scores1), scores2(other.scores2), scores3(other.scores3), empty(other.empty){}
-	void add_scores(char sc1, char sc2, char sc3);
+	void add_scores(int sc1, int sc2, int sc3);
 	int get_game_scores(int number_of_prisoner) const;
-	void extract_matrix(std::ifstream & file);
-	void dump_matrix() const;
 	void print_move_scores() const;
 	void print_game_scores() const;
 	bool is_empty() const { return empty;}
@@ -34,9 +30,9 @@ private:
 	string strategy_name2;
 	string strategy_name3;
 
-	vector<char> scores1;
-	vector<char> scores2;
-	vector<char> scores3;
+	vector<int> scores1;
+	vector<int> scores2;
+	vector<int> scores3;
 
 	bool empty = true;
 };
